6749f79aa3e01c096197ba59ff8e1f9c1f1b1723
Kumbbar <damirnasibullin8@gmail.com>
2023-05-27 23:32:48 +0500

    (start project) 0.1 added base logic to auth service

--- auth/users/tests.py
+++ auth/users/tests.py
@@ -1,26 +1,10 @@
-import os
-import django
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", ".settings")
-django.setup()
-
-
-
-
-import sys
-
-project = os.path.dirname(os.path.abspath('manage.py'))
-sys.path.append(project)
-os.environ["DJANGO_SETTINGS_MODULE"] = "/mnt/c/Pycharm_Projects/Django/restaurant/auth/auth/settings.py"
-
-
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 
 
-class AccountTests(APITestCase):
-    def test_create_account(self):
+class UserTests(APITestCase):
+    def test_create_user_bad_name(self):
         """
         Ensure we can create a new account object.
         """
@@ -28,3 +12,21 @@ class AccountTests(APITestCase):
         data = {'username': 'test', 'password': 'Good34!pass4'}
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_user_bad_password(self):
+        """
+        Ensure we can create a new account object.
+        """
+        url = reverse('register')
+        data = {'username': 'interesting_name8', 'password': 'badpass'}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_user_good_data(self):
+        """
+        Ensure we can create a new account object.
+        """
+        url = reverse('register')
+        data = {'username': 'interesting_name8', 'password': 'GooDPa33W*'}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
